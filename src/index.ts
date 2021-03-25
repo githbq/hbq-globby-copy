c81713cd5b11efd6dabd68f9a1d0ddb3c9368b4c
hubiqing <hubiqing@jd.com>
2021-03-25 16:12:28 +0800

    cwd

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,16 @@ import * as globby from 'globby'
 const replacePathSplit = (path) => path.replace(/\\/g, '/')
 
 export default async (patterns, targetPath, options?, successCallback?, errorCallback?) => {
-    const newTargetPath = replacePathSplit(path.resolve(targetPath))
+    options.cwd = options.cwd || process.cwd()
+    const isAbsolute = path.isAbsolute(targetPath)
+    const newTargetPath = replacePathSplit(isAbsolute ? targetPath : path.join(options.cwd, targetPath))
     await fs.ensureDir(newTargetPath)
 
     const filePaths = await globby([].concat(patterns || '**/*'), { dot: true, ...options, absolute: false })
     if (!targetPath) return
     const promises = filePaths.map(async (filePath, i) => {
         try {
-            await fs.copy(filePath, path.resolve(newTargetPath, filePath))
+            await fs.copy(filePath, path.join(options.cwd, newTargetPath, filePath))
             successCallback && successCallback(filePath, i)
         } catch (e) {
             errorCallback && errorCallback(e, filePath, i)
